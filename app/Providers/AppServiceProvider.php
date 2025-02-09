51974cd4b4d6d89e14a114157c5c0466f3a1c31d
Vova1408zp <neatcoding@gmail.com>
2025-02-09 18:21:57 +0200

    fix AppServiceProvider to version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+      if ($this->app->isProduction()) {
+        URL::forceHttps();
+      }
     }
 }
